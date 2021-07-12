aa5f364170290d12ddb9826f4e82c8a37b24a05a
Prachi Vishnoi <prachi27vishnoi@gmail.com>
2021-07-12 17:54:08 +0530

    feat: cropped screenshot, different screenshot

--- extractText.js
+++ extractText.js
@@ -1,11 +1,16 @@
 const getText = require("./googleVisionAPI");
 const { exec, execSync } = require("child_process");
+const { readFile, unlink } = require("fs").promises;
+const Jimp = require("jimp");
 
 //  API KEY file path
 let API_KEY_PATH = undefined;
 
 // Emr configs
 let emrConfigs = [];
+
+// Jimp object representing last screenshot taken
+let lastScreenshot = null;
 
 // Osascript to detect title of active window in case the active win fails
 const osascript = `
@@ -25,9 +30,9 @@ end tell
 return {windowTitle}
 `;
 
-const set_api_key = path => API_KEY_PATH = path;
+const set_api_key = (path) => (API_KEY_PATH = path);
 
-const set_config = emrconfig => emrConfigs = emrconfig.config; 
+const set_config = (emrconfig) => (emrConfigs = emrconfig.config);
 
 /**
  * Converts a rect buffer to a rect object
@@ -64,7 +69,7 @@ const getActiveWindowWin32 = () => {
 
   const title = titleBuffer.toString("ucs-2").replace(/\0/g, "");
   const bounds = rectBufferToObject(rectBuffer);
-  
+
   return { title, bounds };
 };
 
@@ -82,7 +87,7 @@ const detectWindow = () => {
     } else if (process.platform === "darwin" || process.platform === "linux") {
       window = require("active-win").sync();
     }
-    
+
     let { title } = window;
     const { bounds, id } = window;
 
@@ -94,12 +99,12 @@ const detectWindow = () => {
       } catch {}
     }
 
-    if(emrConfigs.length === 0) {
+    if (emrConfigs.length === 0) {
       return {
         windowId: id,
         windowTitle: title,
-        windowBounds: bounds
-      }
+        windowBounds: bounds,
+      };
     }
 
     // Check if title matches any emr
@@ -126,6 +131,24 @@ const detectWindow = () => {
 };
 
 /**
+ * Crops screenshot according to crop percentages
+ * @param {Object} img Jimp image object
+ * @param {Object} cropPercentages Crop percentages
+ * @return {Object} Jimp image object
+ */
+const cropScreenshot = (img, cropPercentages) => {
+  // Finding bounding box
+  const { width, height } = img.bitmap;
+  const { left, top, right, bottom } = cropPercentages;
+  const x = Math.floor((left * width) / 100);
+  const y = Math.floor((top * height) / 100);
+  const w = Math.ceil((right * width) / 100 - x);
+  const h = Math.ceil((bottom * height) / 100 - y);
+
+  return img.crop(x, y, w, h);
+};
+
+/**
  * Take screenshot of the windows with given window bounds on windows os
  * @param {Object} bounds Window bounds
  * @return {Object} Screenshot in the form of Jimp image object
@@ -135,7 +158,6 @@ const grabScreenshotWindows = ({ left, top, right, bottom }) => {
   const y = top;
   const width = right - top;
   const height = bottom - top;
-  const Jimp = require("jimp");
   const screenshotDesktop = require("screenshot-desktop");
   return screenshotDesktop()
     .then((img) => {
@@ -178,7 +200,7 @@ const grabScreenshotMac = (windowId) => {
 
 const grabScreenshotLinux = (windowId) => {
   return new Promise((resolve, reject) => {
-    let img;
+    let screenshot = null;
 
     const tempPath = `${new Date().valueOf()}.jpg`;
 
@@ -187,15 +209,18 @@ const grabScreenshotLinux = (windowId) => {
         reject(error);
       }
 
-      const { readFile, unlink } = require("fs").promises;
-
       readFile(tempPath)
-        .then((file) => {
-          img = Buffer.from(file).toString("base64");
+        .then((img) => {
+          screenshot = img;
           // Delete saved screenshot
           return unlink(tempPath);
         })
-        .then(() => resolve(img))
+        // Create Jimp object
+        .then(() => Jimp.read(screenshot))
+        // Scale down screenshot
+        .then((img) => img.scale(0.5))
+
+        .then((img) => resolve(img))
         .catch((err) => reject(err));
     });
   });
@@ -212,24 +237,67 @@ const getImage = (windowId, windowBounds) => {
   }
 };
 
+/**
+ * Finds whether the current screenshot is different from the last screenshot
+ * @param {Object} screenshot Jimp image object
+ * @return {boolean} Whether the screenshot is different
+ */
+const isScreenshotDifferent = (screenshot) => {
+  if (!screenshot || !lastScreenshot) return true;
+  return Boolean(Jimp.diff(screenshot, lastScreenshot, 0).percent);
+};
+
 const extractText = async () => {
-  if(API_KEY_PATH === undefined) {
-    return "Api key path not found";
+  // check if API KEY is set or not
+  if (API_KEY_PATH === undefined) {
+    return new Error("Api key path not found");
   }
+
   const window = detectWindow();
 
   if (window) {
     try {
-      let img = await getImage(window.windowId, window.windowBounds);
-      if(!img) return;
-      let texts = await getText(API_KEY_PATH, img);
-      if(!texts) return;
-      return texts; 
+      let image = await getImage(window.windowId, window.windowBounds);
+
+      // image can't be taken
+      if (!image) {
+        return new Error("Screenshot can't be taken");
+      }
+
+      if (window.cropPercentages) {
+        // Crop the screenshot
+        const croppedScreenshot = cropScreenshot(
+          image.clone(), // Sending clone of origional screenshot as we need origional screenshot
+          window.cropPercentages
+        );
+
+        // Compare cropped screenshot with last croppped screenshot
+        if (!isScreenshotDifferent(croppedScreenshot)) {
+          // Screenshot were same
+          return new Error("Screenshot same as previous screenshot");
+        }
+
+        // Save cropped screenshot as lastScreenshot for comparision in next iteration
+        lastScreenshot = croppedScreenshot;
+      }
+      // Return origional screenshot in the form of base64 string
+      image = await image.getBase64Async("image/png");
+
+      // .then((img) => img = Buffer.from(file).toString("base64"))
+
+      image = image.replace("data:image/png;base64,", "");
+
+      let texts = await getText(API_KEY_PATH, image);
+
+      if (!texts) {
+        return new Error();
+      }
+
+      return texts;
     } catch (e) {
-      console.error(e);
-      return;
+      return e;
     }
   }
 };
 
-module.exports = { set_api_key, set_config, extractText};
+module.exports = { set_api_key, set_config, extractText };
